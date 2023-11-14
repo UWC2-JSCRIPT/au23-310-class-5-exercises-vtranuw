d6599f3c991cf25350f546228f5c470aab7ebeaa
vtranuw <147575564+vtranuw@users.noreply.github.com>
2023-11-14 13:44:12 -0800

    Update stoppingBehavior.js
    Finished by Viet T

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,13 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
-
+document.getElementById('more-info').addEventListener('click', (event) => {
+  event.preventDefault(); // This will stop the default navigation behavior
+  alert("Here's some info");
+});
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+document.getElementById('dog').addEventListener('click', (event) => {
+  event.stopPropagation(); // This will stop the event from propagating to the 'cat' container
+  alert('Bow wow!');
+});
