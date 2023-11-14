3e3854af6c36e1992c2c08654856a4a3ef39e12b
vtranuw <147575564+vtranuw@users.noreply.github.com>
2023-11-14 13:41:45 -0800

    Update plusesAndMinuses.js

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,18 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
+const updateCount = (amount) => {
+  const countDisplay = document.getElementById("count");
+  let currentCount = parseInt(countDisplay.textContent);
+  currentCount += amount;
+  countDisplay.textContent = currentCount;
+};
+// Set up event listeners
+document.addEventListener("DOMContentLoaded", () => {
+ // When a user clicks the + element, the count should increase by 1 on screen.
+    .getElementById("plus")
+    .addEventListener("click", () => updateCount(1));
+
+// When a user clicks the – element, the count should decrease by 1 on screen.
+    .getElementById("minus")
+    .addEventListener("click", () => updateCount(-1));
+});
