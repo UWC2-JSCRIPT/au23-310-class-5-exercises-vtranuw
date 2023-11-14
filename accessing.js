e3194a060c34d4515f82e7781ab4a58f9c042108
vtranuw <147575564+vtranuw@users.noreply.github.com>
2023-11-14 13:38:19 -0800

    Update accessing.js
    Finished by Viet T

--- accessing.js
+++ accessing.js
@@ -1,5 +1,13 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
-
+document.getElementById("weather-head").textContent =
+  "February 10 Weather Forecast, Seattle";
 // Change the styling of every element with class "sun" to set the color to "orange"
+document.querySelectorAll(".sun").forEach( el => {
+  el.style.color = "orange";
+});
 
 // Change the class of the second <li> from to "sun" to "cloudy"
+let sunElements = document.querySelectorAll(".sun");
+if (sunElements.length > 1) {
+  sunElements[1].className = "cloudy";
+}
